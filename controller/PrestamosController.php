0c4ed8fa7172a78c1eb2e0bdb1f113ca56ce8277
Adrian Moreno Gomez <156430599+AdrianMoreno19@users.noreply.github.com>
2025-03-11 19:37:05 +0100

    Delete controller directory

--- controller/PrestamosController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Prestamos extends Controller {
-    public function index() {}
-    public function listar() {}
-    public function registrar() {}
-    public function devolver() {}
-    public function pdf() {}
-}
